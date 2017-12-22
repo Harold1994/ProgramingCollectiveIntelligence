be98f014ea6e53334d4b5c5c45699bff08511094
lihe <lihe12@baidu.com>
2017-12-22 16:00:34 +0800

    添加了K-means聚类

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,3 +1,4 @@
+import random
 from math import sqrt
 from PIL import Image, ImageDraw
 
@@ -14,20 +15,26 @@ def readfile(filename):
     return rownames, colnames, data
 
 
+
 def pearson(v1, v2):
+    # Simple sums
     sum1 = sum(v1)
     sum2 = sum(v2)
 
+    # Sums of the squares
     sum1Sq = sum([pow(v, 2) for v in v1])
     sum2Sq = sum([pow(v, 2) for v in v2])
 
+    # Sum of the products
     pSum = sum([v1[i] * v2[i] for i in range(len(v1))])
 
+    # Calculate r (Pearson score)
     num = pSum - (sum1 * sum2 / len(v1))
     den = sqrt((sum1Sq - pow(sum1, 2) / len(v1)) * (sum2Sq - pow(sum2, 2) / len(v1)))
-    if den == 0:
-        return 0
+    if den == 0: return 0
+
     return 1.0 - num / den
+
 
 
 class bicluster:
@@ -42,25 +49,43 @@ class bicluster:
 def hcluster(rows, distance=pearson):
     distances = {}
     currentclustid = -1
+
+    # Clusters are initially just the rows
     clust = [bicluster(rows[i], id=i) for i in range(len(rows))]
+
     while len(clust) > 1:
         lowestpair = (0, 1)
         closest = distance(clust[0].vec, clust[1].vec)
+
+        # loop through every pair looking for the smallest distance
         for i in range(len(clust)):
             for j in range(i + 1, len(clust)):
+                # distances is the cache of distance calculations
                 if (clust[i].id, clust[j].id) not in distances:
                     distances[(clust[i].id, clust[j].id)] = distance(clust[i].vec, clust[j].vec)
+
                 d = distances[(clust[i].id, clust[j].id)]
-            if d < closest:
-                closest = d
-                lowestpair = (i, j)
-        mergevec = [(clust[lowestpair[0]].vec[i] + clust[lowestpair[1]].vec[i]) / 2.0 for i in range(len(clust[0].vec))]
-        newcluster = bicluster(mergevec, left=clust[lowestpair[0]], right=clust[lowestpair[1]], distance=closest,
-                               id=currentclustid)
+
+                if d < closest:
+                    closest = d
+                    lowestpair = (i, j)
+
+        # calculate the average of the two clusters
+        mergevec = [
+            (clust[lowestpair[0]].vec[i] + clust[lowestpair[1]].vec[i]) / 2.0
+            for i in range(len(clust[0].vec))]
+
+        # create the new cluster
+        newcluster = bicluster(mergevec, left=clust[lowestpair[0]],
+                               right=clust[lowestpair[1]],
+                               distance=closest, id=currentclustid)
+
+        # cluster ids that weren't in the original set are negative
         currentclustid -= 1
         del clust[lowestpair[1]]
         del clust[lowestpair[0]]
         clust.append(newcluster)
+
     return clust[0]
 
 
@@ -129,8 +154,56 @@ def drawnode(draw, clust, x, y, scaling, labels):
         draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
 
 
+def kcluster(rows, distance=pearson, k=4):
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))] for j in range(k)]
+    lastmatches = None
+    for t in range(100):
+        print("iteration %d" % t)
+        bestmatches = [[] for i in range(k)]
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row):
+                    bestmatch = i
+            bestmatches[bestmatch].append(j)
+        if bestmatches == lastmatches:
+            break
+        lastmatches = bestmatches
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                    for j in range(len(avgs)):
+                        avgs[j] /= len(bestmatches[i])
+                    clusters[i] = avgs
+    return bestmatches
 
-blognames, words, data = readfile('data/blogdata.txt')
-clust = hcluster(data)
 
-drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+def tanimoto(v1, v2):
+    c1, c2, shr = 0, 0, 0
+    for i in range(len(v1)):
+        if v1[i] != 0:
+            c1 += 1
+        if v2[i] != 0:
+            c2 += 1
+        if v1[i] != 0 and v2[i] != 0:
+            shr += 1
+    return 1 - (float(shr) / (c1 + c2 - shr))
+
+
+wants, people, data = readfile('data/zebo.txt')
+clust = hcluster(data, distance=tanimoto)
+drawdendrogram(clust, wants)
+# blognames, words, data = readfile('data/blogdata.txt')
+# kcluste = kcluster(data,k=4)
+# print([[blognames[r] for r in kcluste[i]] for i in range(4)],end='\n')
+
+# clust = hcluster(data)
+# drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
