74b2c93229f5eeaef7321759ee56bd569776e9c3
lihe <lihe12@baidu.com>
2018-01-05 20:19:42 +0800

    添加基于内容的排名和pagerank等算法

--- chapter4/main.py
+++ chapter4/main.py
@@ -1,6 +1,12 @@
 from chapter4 import searchengine
+from chapter4 import nn
 
-clawer = searchengine.crawler('searchindex.db')
-clawer.calculatepagerank()
+# clawer = searchengine.crawler('searchindex.db')
+# clawer.calculatepagerank()
 # e = searchengine.searcher('searchindex.db')
 # e.query("functional programming")
+
+mynet = nn.searchnet('nn.db')
+# mynet.maketables()
+mynet.generatehiddennode([101, 103], [201, 202, 203])
+print(mynet.getresult([101, 103], [201, 202, 203]))
--- chapter4/searchengine.py
+++ chapter4/searchengine.py
@@ -231,8 +231,9 @@ class searcher:
         return self.normalizescores(inboundcount)
 
     def pagerankscore(self, rows):
-        pageranks = dict([(row[0], self.con.execute('select score from pagerank where urlid=%d' % row[0]).fetchone()[0]
-                           for row in rows)])
+        pageranks = dict(
+            [(row[0], self.con.execute('select score from pagerank where urlid=%d' % row[0]).fetchone()[0]) for row in
+             rows])
         maxrank = max(pageranks.values())
         normalizedscores = dict([(u, float(l) / maxrank) for (u, l) in pageranks.items()])
         return normalizedscores
